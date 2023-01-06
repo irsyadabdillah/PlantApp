59cfb3efe5864d185a7206d600ebec83f61b2321
Irsyad Abdillah <irsyad.abdillah@ist.id>
2023-01-06 16:21:25 +0700

    Initial Commit

---
+++ PlantApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlantApp
+//
+//  Created by Irsyad Abdillah on 06/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlantApp/PlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAppApp.swift
+//  PlantApp
+//
+//  Created by Irsyad Abdillah on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
